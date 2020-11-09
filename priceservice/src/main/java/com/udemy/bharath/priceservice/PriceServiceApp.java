78c96a71059f634e58ad0db1b5f174bdd13caaa9
Amir Yunas <ayunas@gmail.com>
2020-11-08 20:25:55 -0600

    got price service up and running.  made sure to add the application properties and the pom xml dependencies.

--- priceservice/src/main/java/com/udemy/bharath/priceservice/PriceServiceApp.java
+++ priceservice/src/main/java/com/udemy/bharath/priceservice/PriceServiceApp.java
@@ -2,12 +2,14 @@ package com.udemy.bharath.priceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class PriceserviceApplication {
+public class PriceServiceApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PriceserviceApplication.class, args);
+		SpringApplication.run(PriceServiceApp.class, args);
 		System.out.println("Price Microservice Running on port : 7070");
 	}
 
